02be4610c380442b13811e6dc42831079150e16c
Petar Ivanov <peter.ivanov89@gmail.com>
2013-12-18 05:32:16 +0200

    visualization

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
 CXX = g++
+SKETCH = best
 
-compile: ga
+compile: ga.cpp
 	${CXX} ga.cpp -o ga
 
-run: 
-	./ga ../Data/2000HopperM/smallbins.csv ../Data/2000HopperM/smallitems.csv
+run: ga
+	./ga ../Data/2000HopperM/smallbins.csv ../Data/2000HopperM/smallitems.csv ${SKETCH}.sps 2> tmp 
+
+visualize:
+	./sketchps <${SKETCH}.sps >${SKETCH}.eps
--- ga.cpp
+++ ga.cpp
@@ -18,6 +18,7 @@ struct Item {
   int id;
   int h, w;
 
+  Item() {}
   Item(int _id, int _h, int _w)
     : id(_id), h(_h), w(_w) {}
 };
@@ -25,6 +26,7 @@ struct Item {
 // not to be changed after read
 vector<Bin> bins;
 vector<Item> items;
+int items_surface;
 
 // GA individual as defined by (+rotation & multibins)
 // HEURISTICS FOR LARGE STRIP PACKING PROBLEMS
@@ -72,6 +74,7 @@ struct Tree {
     Node *first, *second; // children
     int cut; // if not leaf
 
+    Node() {}
     Node(Item _item, Node *_first, Node *_second, int _cut=0)
       : item(_item), first(_first), second(_second), cut(_cut) {}
   };
@@ -79,8 +82,11 @@ struct Tree {
   int n;
   vector<int> postfix; // includes HORIZONTAL, VERTICAL, 0, ..., n-1
   vector<bool> rots;
+  vector<Item> plot; // absolute lower-left positions of items
   Node *root;
   int h, w;
+
+  Tree() : root(NULL) {}
 
   Tree(State &state) {
     n = state.perm.size();
@@ -91,6 +97,34 @@ struct Tree {
     w = size.first;
     h = size.second;
     fprintf(stderr, "size: (%d, %d)\n", w, h);
+  }
+
+  Tree(const Tree &other) {
+    *this = other;
+  }
+
+  Tree& operator=(const Tree& other) {
+    n = other.n;
+    postfix = other.postfix;
+    rots = other.rots;
+    plot = other.plot;
+    root = rec_copy(other.root);
+    h = other.h;
+    w = other.w;
+
+    return *this;
+  }
+
+  Node *rec_copy(Node *other_v) {
+    if (other_v == NULL)
+      return NULL;
+
+    Node *v = new Node(*other_v);
+
+    v->first  = rec_copy(other_v->first);
+    v->second = rec_copy(other_v->second);
+
+    return v;
   }
 
   int fitness() {
@@ -161,7 +195,9 @@ struct Tree {
 
         node = new Node(item, first, second, op);
       } else {
-        node = new Node(items[op], NULL, NULL);
+        Item item = items[op];
+        if (rots[op]) swap(item.w, item.h);
+        node = new Node(item, NULL, NULL);
       }
 
       curr_operands.push_back(node);
@@ -176,6 +212,7 @@ struct Tree {
       fprintf(stderr, "%*s", shift, "");
       if (v->first == NULL && v->second == NULL) {
         Item item = v->item;
+        plot.push_back( Item(item.id, dx, dy) );
         fprintf(stderr, "leaf: #%d (%d, %d -- %d, %d)\n", item.id, dx, dy, dx + item.w, dy + item.h);
         return make_pair(item.w, item.h);
       } else {
@@ -249,30 +286,82 @@ void read_items(char *infile) {
   int m;
   int id, h, w;
 
+  items_surface = 0;
   while (fscanf(in, "%d,%d,%d", &id, &w, &h) != EOF) {
     Item item(items.size(), h, w);
     items.push_back(item);
+    items_surface += h*w;
   }
 
   assert(!items.empty());
 }
 
-void ga() {
-  State curr_state(items.size());
-  Tree curr_tree(curr_state);
-  
+void visualize(Tree &tree, char *sketchps_fn) {
+  FILE *fout = fopen(sketchps_fn, "w");
+
+  fprintf(fout, "colour green\n");
+  fprintf(fout, "polygon\n");
+  fprintf(fout, "%d %d\n", 0, 0);
+  fprintf(fout, "%d %d\n", 0, tree.h);
+  fprintf(fout, "%d %d\n", tree.w, tree.h);
+  fprintf(fout, "%d %d\n", tree.w, 0);
+  fprintf(fout, "\n");
+
+  fprintf(fout, "colour black\n");
+  for (int i=0; i<tree.plot.size(); i++) {
+    Item item = tree.plot[i]; // absolute coordinates
+    int dw = items[item.id].w;
+    int dh = items[item.id].h;
+
+    fprintf(fout, "polyline\n");
+    fprintf(fout, "%d %d\n", item.w, item.h);
+    fprintf(fout, "%d %d\n", item.w, item.h+dh);
+    fprintf(fout, "%d %d\n", item.w+dw, item.h+dh);
+    fprintf(fout, "%d %d\n", item.w+dw, item.h);
+    fprintf(fout, "%d %d\n", item.w, item.h);
+    fprintf(fout, "%d %d\n", item.w+dw, item.h+dh);
+    fprintf(fout, "%d %d\n", item.w+dw, item.h);
+    fprintf(fout, "%d %d\n", item.w, item.h+dh);
+    fprintf(fout, "\n");
+  }
+}
+
+Tree ga() {
+  int best_fitness = int(1e8);
+  Tree best_tree;
+
+  for(int i=0; i<3000; i++) {
+    State state(items.size(), true, false);
+    Tree tree(state);
+ 
+    int fitness = tree.fitness();
+    if (fitness < best_fitness) {
+      best_fitness = fitness;
+      best_tree = tree;
+    }
+
+    printf("fitness #%d: %d(%.1lf%%)\n", i, fitness, 100.0*items_surface/fitness);
+  }
+
+  return best_tree;
 }
 
 int main(int argn, char *args[])
 {
-  if (argn != 3) {
-    fprintf(stderr, "args: <bins_file> <items_file>\n");
+  if (argn != 4) {
+    fprintf(stderr, "args: <bins_file> <items_file> <sketchps_file>\n");
   }
   
   read_bins(args[1]);
   read_items(args[2]);
 
-  ga();
+  Tree best_tree = ga();
+  visualize(best_tree, args[3]);
+
+  best_tree.print_tree(best_tree.root, 0, 0);
+  int best_fitness = best_tree.fitness();
+
+  printf("best fitness: %d(%.1lf%%)\n", best_fitness, 100.0*items_surface/best_fitness);
   
   return 0;
 }
